5e7ed810dee72db4d578011771ccb046a972b043
Javier Flores <xavifs89@gmail.com>
2022-04-29 10:46:07 -0600

    [module-03] Creating the Main React Component

---
+++ client/client.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>This is a React component!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("Container"));
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import { readFileSync } from "fs";
 
 const app = new express();
 
+app.use(express.static("dist"));
+
 app.get("/", async (_req, res) => {
-  res.send("<h1>REACT IS EXCELENT</h1>");
+  const index = readFileSync("public/index.html", "utf8");
+  res.send(index);
 });
 
 app.listen(7777);
