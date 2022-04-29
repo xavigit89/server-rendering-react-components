79487de1a6e04e3d2e4b4c7f3471c5d4486675df
Javier Flores <xavifs89@gmail.com>
2022-04-29 11:19:27 -0600

    [module-04] Adding Functionality to Our Application

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const App = () => {
-  return <div>This is a React component!</div>;
+const App = ({ questions, answers }) => {
+  return (
+    <div>
+      <h1>Q&A Tool</h1>
+      {questions.map(({ questionId, content }) => (
+        <div key={questionId}>
+          <h3>{content}</h3>
+          <div>
+            {answers
+              .filter((answer) => answer.questionId === questionId)
+              .map(({ answerId, content, upvotes }) => (
+                <div key={answerId}>
+                  <span>
+                    {content} - {upvotes}
+                  </span>
+                </div>
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -5,13 +5,52 @@ import { renderToString } from "react-dom/server";
 
 import App from "../client/App";
 
+const data = {
+  questions: [
+    {
+      questionId: "Q1",
+      content: "Should we use Jquery or Fetch for Ajax?",
+    },
+    {
+      questionId: "Q2",
+      content: "What is the best feature of React?",
+    },
+  ],
+  answers: [
+    {
+      answerId: "A1",
+      questionId: "Q1",
+      upvotes: 2,
+      content: "JQuery",
+    },
+    {
+      answerId: "A2",
+      questionId: "Q1",
+      upvotes: 1,
+      content: "Fetch",
+    },
+    {
+      answerId: "A3",
+      questionId: "Q2",
+      upvotes: 1,
+      content: "Performance",
+    },
+    {
+      answerId: "A4",
+      questionId: "Q2",
+      upvotes: 3,
+      content: "Ease of use",
+    },
+  ],
+};
+
 const app = new express();
 
 app.use(express.static("dist"));
 
 app.get("/", async (_req, res) => {
   const index = readFileSync("public/index.html", "utf8");
-  const rendered = renderToString(<App />);
+  const rendered = renderToString(<App {...data} />);
   res.send(index.replace("{{rendered}}", rendered));
 });
 
